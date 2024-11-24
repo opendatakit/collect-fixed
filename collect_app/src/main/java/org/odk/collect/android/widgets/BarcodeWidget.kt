5f8df15634432495555ae4d72c283981d0fa0e5b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-11-24 13:23:58 +0100

    Renamed the widget layout file

--- collect_app/src/main/java/org/odk/collect/android/widgets/BarcodeWidget.kt
+++ collect_app/src/main/java/org/odk/collect/android/widgets/BarcodeWidget.kt
@@ -10,7 +10,7 @@ import org.javarosa.core.model.data.IAnswerData
 import org.javarosa.core.model.data.StringData
 import org.javarosa.form.api.FormEntryPrompt
 import org.odk.collect.android.activities.ScannerWithFlashlightActivity
-import org.odk.collect.android.databinding.BarcodeWidgetAnswerBinding
+import org.odk.collect.android.databinding.BarcodeWidgetBinding
 import org.odk.collect.android.formentry.questions.QuestionDetails
 import org.odk.collect.android.utilities.Appearances
 import org.odk.collect.android.utilities.Appearances.hasAppearance
@@ -29,14 +29,14 @@ class BarcodeWidget(
     private val waitingForDataRegistry: WaitingForDataRegistry,
     private val cameraUtils: CameraUtils
 ) : QuestionWidget(context, questionDetails), WidgetDataReceiver {
-    lateinit var binding: BarcodeWidgetAnswerBinding
+    lateinit var binding: BarcodeWidgetBinding
 
     init {
         render()
     }
 
     override fun onCreateAnswerView(context: Context, prompt: FormEntryPrompt, answerFontSize: Int): View {
-        binding = BarcodeWidgetAnswerBinding.inflate((context as Activity).layoutInflater)
+        binding = BarcodeWidgetBinding.inflate((context as Activity).layoutInflater)
 
         if (prompt.isReadOnly) {
             binding.barcodeButton.visibility = GONE
